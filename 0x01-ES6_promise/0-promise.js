00a97daf3323d3d29f65bad1bcc5ce187af3aad1
Mikelonia <onajitemike4real@gmail.com>
2023-09-26 17:09:05 +0000

    Task 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,2 @@
-function getResponseFromAPI() {
-  return new Promise(resolve => {
-    // Returning a promise using the provided prototype
-    resolve();
-  });
-}
-
-export default getResponseFromAPI;
+export default () => Promise.resolve();
 
