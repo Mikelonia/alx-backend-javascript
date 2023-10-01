8ef764405b8ca2c81bbab9c10c61f94ea8862dda
Mikelonia <onajitemike4real@gmail.com>
2023-10-01 20:23:11 +0000

    Task 0-classroom.js

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Represents a class called room. By Okpako Michael
+ */
+export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @parameter {Number} maxStudentsSize - The maximum number of
+   * students in the class presented.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
