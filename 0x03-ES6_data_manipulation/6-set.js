368721e412ba42ee4e498db250764fdb39b0a785
Mikelonia <onajitemike4real@gmail.com>
2023-10-02 18:37:39 +0000

    removal of trailing whitespace

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Michael Okpako <https://github.com/Mikelonia>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
